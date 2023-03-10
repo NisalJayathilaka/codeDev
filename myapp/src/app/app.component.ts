d0a4a7914b976ec2c63e6941cace19b026d60e32
NisalJayathilaka <nisal7075399@gmail.com>
2023-03-10 18:40:09 +0530

    adding changes

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,23 +1,26 @@
 import { Component } from '@angular/core';
 import { Payment } from './payment';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-    
+  payment: Payment[] = [];
+  sortedArray:Payment[] = [];
   
-  payment:Payment[] =[];
 
-    submitValues(f:any):void{
-        console.log('asasas');
-        this.payment.push(f.value);
-        console.log(this.payment); 
-    }
+  submitValues(f: any): void {
+    console.log('asasas');
+    this.payment.push(f.value);
+    this.sortedArray = _.sortBy(this.payment, 'name');
+    console.log(this.payment);
+    console.log(this.sortedArray);
+  }
 
-    deleteValue(){
-      this.payment.pop();
-    }
+  deleteValue() {
+    this.payment.pop();
+  }
 }
