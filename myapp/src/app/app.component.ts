8b11d16f451a4fe45dac3a3b3121adcfe863df2e
NisalJayathilaka <nisal7075399@gmail.com>
2023-03-10 18:42:26 +0530

    adding changes

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -10,17 +10,13 @@ import * as _ from 'underscore';
 export class AppComponent {
   payment: Payment[] = [];
   sortedArray:Payment[] = [];
-  
 
   submitValues(f: any): void {
-    console.log('asasas');
     this.payment.push(f.value);
     this.sortedArray = _.sortBy(this.payment, 'name');
-    console.log(this.payment);
-    console.log(this.sortedArray);
   }
 
   deleteValue() {
-    this.payment.pop();
+    this.sortedArray.pop();
   }
 }
